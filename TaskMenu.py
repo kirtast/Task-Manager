655f028717ce5f0a21428c4dee254b4d8d84a9de
kirtast <jpon.her@gmail.com>
2020-03-09 18:36:12 +0100

    Testing the main window for TaskMenu

---
+++ TaskMenu.py
@@ -0,0 +1,42 @@
+
+
+from tkinter import *
+import tkinter.ttk as ttk
+import datetime
+import sqlite3
+import urllib.request, urllib.parse
+import datetime
+from pandas import DataFrame
+import pandas as pd
+from tkcalendar import Calendar, DateEntry
+
+
+def main():
+  root=Tk()
+  root.title('TASK MENU')
+
+  root.resizable(0,0)
+  #root.iconbitmap('seat_logo.ico')
+
+  frameMain=Frame(root)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
